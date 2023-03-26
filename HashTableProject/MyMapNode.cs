15ad78a9bb57f87c31a7db9d35cda9686e63a809
Poojachakor <chakorpooja07@gmail.com>
2023-03-26 13:26:12 +0530

    Add: UC3 file

--- HashTableProject/MyMapNode.cs
+++ HashTableProject/MyMapNode.cs
@@ -6,130 +6,122 @@ using System.Threading.Tasks;
 
 namespace HashTableProject
 {
-   
-        internal class MyMapNode<K, V>
+
+    internal class MyMapNode<K, V>
+    {
+        public struct KeyValue<k, v>
         {
-            public struct KeyValue<k, v>
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+        private readonly int size;
+        //int[] arr;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            //arr=new int[size];
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int hash = key.GetHashCode(); //637362
+            int position = hash % size; // 0 to 4
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
             {
-                public k Key { get; set; }
-                public v Value { get; set; }
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+        public LinkedList<KeyValue<K, V>> GetArrayPositionAndLinkedList(K key)
+        {
+            int position = GetArrayPosition(key); //index number of array
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            return linkedList;
+        }
+        public V Get(K key)
+        {
+            var linkedList = GetArrayPositionAndLinkedList(key);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
             }
 
-            private readonly int size;
-            //int[] arr;
-            private readonly LinkedList<KeyValue<K, V>>[] items;
-
-            public MyMapNode(int size)
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            var linkedList = GetArrayPositionAndLinkedList(key);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            { Key = key, Value = value };
+            if (linkedList.Count != 0)
             {
-                this.size = size;
-                //arr=new int[size];
-                this.items = new LinkedList<KeyValue<K, V>>[size];
-            }
-            protected int GetArrayPosition(K key)
-            {
-                int hash = key.GetHashCode(); //637362
-                int position = hash % size; // 0 to 4
-                return Math.Abs(position);
-            }
-
-            public V Get(K key)
-            {
-                var linkedList = GetArrayPositionAndLinkedList(key);
-                foreach (KeyValue<K, V> item in linkedList)
+                foreach (KeyValue<K, V> item1 in linkedList)
                 {
-                    if (item.Key.Equals(key))
-                        return item.Value;
-                }
-
-                return default(V);
-            }
-
-            public void Add(K key, V value)
-            {
-                var linkedList = GetArrayPositionAndLinkedList(key);
-                KeyValue<K, V> item = new KeyValue<K, V>()
-                { Key = key, Value = value };
-                if (linkedList.Count != 0)
-                {
-                    foreach (KeyValue<K, V> item1 in linkedList)
+                    if (item1.Key.Equals(key))
                     {
-                        if (item1.Key.Equals(key))
-                        {
-                            Remove(key);
-                            break;
-                        }
+                        Remove(key);
+                        break;
                     }
                 }
-                linkedList.AddLast(item); // to,2
-                                          // Console.WriteLine(item.Key + " " + item.Value);
             }
+            linkedList.AddLast(item); // to,2
+                                      // Console.WriteLine(item.Key + " " + item.Value);
 
-            public bool Exists(K key)
+        }
+        public void Remove(K key)
+        {
+            var linkedList = GetArrayPositionAndLinkedList(key);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
             {
-                var linkedList = GetArrayPositionAndLinkedList(key);
-                foreach (KeyValue<K, V> item in linkedList)
+                if (item.Key.Equals(key))
                 {
-                    if (item.Key.Equals(key))
-                    {
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            public LinkedList<KeyValue<K, V>> GetArrayPositionAndLinkedList(K key)
-            {
-                int position = GetArrayPosition(key); //index number of array
-                LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
-                return linkedList;
-            }
-
-
-            public void Remove(K key)
-            {
-                var linkedList = GetArrayPositionAndLinkedList(key);
-                bool itemFound = false;
-                KeyValue<K, V> foundItem = default(KeyValue<K, V>);
-                foreach (KeyValue<K, V> item in linkedList)
-                {
-                    if (item.Key.Equals(key))
-                    {
-                        itemFound = true;
-                        foundItem = item;
-                        //linkedList.Remove(item);
-                    }
-                }
-                if (itemFound)
-                {
-                    linkedList.Remove(foundItem);
-                    //Console.WriteLine("Removed successfully with key " + foundItem.Key);
+                    itemFound = true;
+                    foundItem = item;
+                    //linkedList.Remove(item);
                 }
             }
-
-            protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+            if (itemFound)
             {
-                LinkedList<KeyValue<K, V>> linkedList = items[position]; //0
-                if (linkedList == null)
-                {
-                    linkedList = new LinkedList<KeyValue<K, V>>();
-                    items[position] = linkedList;
-                }
-                return linkedList;
-            }
-
-            public void Display()
-            {
-                foreach (var linkedList in items)
-                {
-                    if (linkedList != null)
-                        foreach (var element in linkedList)
-                        {
-                            string res = element.ToString();
-                            if (res != null)
-                                Console.WriteLine(element.Key + " " + element.Value);
-                        }
-                }
+                linkedList.Remove(foundItem);
+                //Console.WriteLine("Removed successfully with key " + foundItem.Key);
             }
         }
+        public bool Exists(K key)
+        {
+            var linkedList = GetArrayPositionAndLinkedList(key);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+    }
     
 }
